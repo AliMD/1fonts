cc680330fbc92e2a6bd5a11eaf291df3c53d1b74
Ali Mihandoost <i@ali.md>
2013-04-27 16:29:20 +0430

    Update demo

---
+++ demo/script.js
@@ -0,0 +1,19 @@
+var
+  fontsList = "Badr, Baran, Bardiya, Compset, Davat, Elham, EsfehanBold, Fantezy, Farnaz, Ferdosi, Hamid, Helal, Homa, JadidBold, Jalal, KoodakBold, Kourosh, Lotus, Mahsa, MehrBold, Mitra, Morvarid, Narm, NasimBold, Nazanin, Roya, SetarehBold, Shiraz, SinaBold, Tabassom, Tehran, TitrBold, TitrTGEBold, Traffic, VahidBold, Yagut, Yas, Yekan, Zar, Ziba".split(', '),
+  $fontSellect = $('#fontSellect'),
+  $fontPreview = $('#fontPreview');
+  $options = '';
+
+for(var i in fontsList){
+  $fontSellect.append(
+    $('<option>')
+      .attr('value',fontsList[i])
+      .html(fontsList[i])
+  );
+
+}
+
+$fontSellect
+  .change(function () {
+    $fontPreview.css('font-family',$(this).val());
+  });
