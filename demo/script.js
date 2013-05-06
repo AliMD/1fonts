9c208f9b5b06df31a34461f8fab1614938076fc7
Ali Mihandoost <i@ali.md>
2013-05-06 09:43:02 +0430

    Add next btn

--- demo/script.js
+++ demo/script.js
@@ -2,11 +2,16 @@ var
   fontsList = "badr, baran, bardiya, compset, davat, elham, esfehan, fantezy, farnaz, ferdosi, hamid, helal, homa, jadid, jalal, koodak, kourosh, lotus, mahsa, mehr, mitra, morvarid, narm, nasim, nazanin, roya, setareh, shiraz, sina, tabassom, tehran, titr, titrtge, traffic, vahid, yagut, yas, yekan, zar, ziba, andalus, arshia, iraniansans, kamran, setareh, tawfig".split(', ').sort(),
   $fontSelect = $('#fontselect'),
   $fontPreview = $('#fontpreview'),
-  currentFont = 0,
+  $nextFont = $('#nextfont'),
+  currentFont = -1,
   changeFont = function (indx) {
+    console.log([currentFont,indx]);
+    if(currentFont === indx) return false;
     $fontPreview
       .removeClass()
       .addClass(fontsList[indx]);
+    $fontSelect.eq(0)[0].selectedIndex = indx;
+    currentFont=indx;
   };
 
 for(var i in fontsList){
@@ -21,6 +26,12 @@ $fontSelect
   .change(function () {
     changeFont(this.selectedIndex);
   });
+
+$nextFont.click(function () {
+  currentFont<fontsList.length-1 || (currentFont = -1);
+  changeFont(currentFont+1);
+  return false;
+});
 
 $('#fontlen')
   .html(fontsList.length);
