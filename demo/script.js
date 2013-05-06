197a2d0ae2417159630aa27d4aaebaa1360b2b34
Ali Mihandoost <i@ali.md>
2013-05-06 09:23:39 +0430

    More demo detail

--- demo/script.js
+++ demo/script.js
@@ -1,18 +1,17 @@
 var
   fontsList = "badr, baran, bardiya, compset, davat, elham, esfehan, fantezy, farnaz, ferdosi, hamid, helal, homa, jadid, jalal, koodak, kourosh, lotus, mahsa, mehr, mitra, morvarid, narm, nasim, nazanin, roya, setareh, shiraz, sina, tabassom, tehran, titr, titrtge, traffic, vahid, yagut, yas, yekan, zar, ziba, andalus, arshia, iraniansans, kamran, setareh, tawfig".split(', ').sort(),
-  $fontSellect = $('#fontSellect'),
-  $fontPreview = $('#fontPreview');
-  $options = '';
+  $fontSelect = $('#fontselect'),
+  $fontPreview = $('#fontpreview');
 
 for(var i in fontsList){
-  $fontSellect.append(
+  $fontSelect.append(
     $('<option>')
       .attr('value',fontsList[i])
       .html(fontsList[i])
   );
 }
 
-$fontSellect
+$fontSelect
   .change(function () {
     $fontPreview
       .removeClass()
